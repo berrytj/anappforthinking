9dc7d3404518cbfb49f07b43dd42e4b9a7b9e2a6
Tom Berry <tberry860@gmail.com>
2013-02-03 15:29:09 -0800

    switched tabs to spaces

--- walls/extra.py
+++ walls/extra.py
@@ -5,76 +5,76 @@ from walls.models import Wall, Mark, Waypoint, Undo, Redo
 def clear_redos():
     '''Clears redo table when user takes any action besides undo.'''
     
-	for redo in Redo.objects.all():
-		redo.delete()
+    for redo in Redo.objects.all():
+        redo.delete()
 
 def save_undo(w, obj, type):
     '''Adds a clone of the object in its former state to the undo table.'''
     
-	undo = Undo(wall = w,
-				obj_pk = obj.pk,
-				text = obj.text,
-				x = obj.x,
-				y = obj.y,
-				type = type)
-	undo.save()
+    undo = Undo(wall = w,
+                obj_pk = obj.pk,
+                text = obj.text,
+                x = obj.x,
+                y = obj.y,
+                type = type)
+    undo.save()
 
 def save_redo(w, obj, type):
     '''Adds a clone of the object in its former state to the redo table.'''
     
-	redo = Redo(wall = w,
-				obj_pk = obj.pk,
-				text = obj.text,
-				x = obj.x,
-				y = obj.y,
-				type = type)
-	redo.save()
+    redo = Redo(wall = w,
+                obj_pk = obj.pk,
+                text = obj.text,
+                x = obj.x,
+                y = obj.y,
+                type = type)
+    redo.save()
 
 def update_obj(obj, text, x, y):
     '''Updates text and location of a mark or waypoint.'''
     
-	obj.text = text
-	obj.x = x
-	obj.y = y
-	obj.save()
+    obj.text = text
+    obj.x = x
+    obj.y = y
+    obj.save()
 
 def create_new(w, text, x, y, type):
     '''Creates a new object with the specified properties.'''
     
-	if type == "mark":
-		return Mark(wall=w, text=text, x=x, y=y)
-	elif type == "waypoint":
-		return Waypoint(wall=w, text=text, x=x, y=y)
+    if type == "mark":
+        return Mark(wall=w, text=text, x=x, y=y)
+    elif type == "waypoint":
+        return Waypoint(wall=w, text=text, x=x, y=y)
 
 def get_obj(w, pk, type): #add try / except clause?
     '''Returns the appropriate object from the database.'''
     
-	if type == "mark":
-		return w.mark_set.get(pk=pk)
-	elif type == "waypoint":
-		return w.waypoint_set.get(pk=pk)
+    if type == "mark":
+        return w.mark_set.get(pk=pk)
+    elif type == "waypoint":
+        return w.waypoint_set.get(pk=pk)
 
 def execute_undo(w, prev, redo):
     '''Updates an object with its former attributes, found in the undo/redo table.'''
     
-	pk = prev.obj_pk
-	type = prev.type
-	obj = get_obj(w, pk, type)
-	if(redo):
-		save_undo(w, obj, type)
-	else:
-		save_redo(w, obj, type)
-	text = prev.text
-	x = prev.x
-	y = prev.y
-	update_obj(obj, text, x, y)
-	prev.delete()
-	render_data = { "pk":pk, "text":text, "x":x, "y":y, "type":type }
-	return render_data
+    pk = prev.obj_pk
+    type = prev.type
+    obj = get_obj(w, pk, type)
+    if(redo):
+        save_undo(w, obj, type)
+    else:
+        save_redo(w, obj, type)
+    text = prev.text
+    x = prev.x
+    y = prev.y
+    update_obj(obj, text, x, y)
+    prev.delete()
+    render_data = { "pk":pk, "text":text, "x":x, "y":y, "type":type }
+    return render_data
 
 def get_user(username):
     '''Returns a user profile, given a username.'''
     
-	user = User.objects.get(username=username)
-	profile = MyProfile.objects.get(user=user)
-	return profile
+    user = User.objects.get(username=username)
+    profile = MyProfile.objects.get(user=user)
+    return profile
--- walls/models.py
+++ walls/models.py
@@ -8,65 +8,65 @@ from accounts.models import MyProfile
 class Wall(models.Model):
     '''A surface for users to create and manipulate marks.'''
     
-	user = models.ForeignKey(User)
-	title = models.CharField(max_length=200)
-	last_updated = models.DateTimeField('last updated', default = datetime.now)
-	def __unicode__(self):
-		return self.title
-	class Meta:
-		get_latest_by = "pk"
-		ordering = ['-id']
+    user = models.ForeignKey(User)
+    title = models.CharField(max_length=200)
+    last_updated = models.DateTimeField('last updated', default = datetime.now)
+    def __unicode__(self):
+        return self.title
+    class Meta:
+        get_latest_by = "pk"
+        ordering = ['-id']
     
 class Mark(models.Model):
     '''A thought, idea, link, or other piece of text created by the user.'''
     
-	wall = models.ForeignKey(Wall)
-	text = models.CharField(max_length=10000, default="", blank=True)
-	x = models.IntegerField()
-	y = models.IntegerField()
-	def __unicode__(self):
-		return self.text
-	class Meta:
-		get_latest_by = "pk"
+    wall = models.ForeignKey(Wall)
+    text = models.CharField(max_length=10000, default="", blank=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    def __unicode__(self):
+	    return self.text
+    class Meta:
+    	get_latest_by = "pk"
 
 class Waypoint(models.Model):
     '''A short piece of text used to denote a category of marks for quick access.'''
     
-	wall = models.ForeignKey(Wall)
-	text = models.CharField(max_length=100, default="", blank=True)
-	x = models.IntegerField()
-	y = models.IntegerField()
-	def __unicode__(self):
-		return self.text
-	class Meta:
-		get_latest_by = "pk"
+    wall = models.ForeignKey(Wall)
+    text = models.CharField(max_length=100, default="", blank=True)
+    x = models.IntegerField()
+    y = models.IntegerField()
+    def __unicode__(self):
+        return self.text
+    class Meta:
+        get_latest_by = "pk"
 
 class Undo(models.Model):
     '''A snapshot of a mark or waypoint containing the pk of its corresponding live object.'''
     
-	wall = models.ForeignKey(Wall) # Probably not necessary since you have the mark/waypoint pk
-	obj_pk = models.IntegerField()
-	text = models.CharField(max_length=10000, default="", blank=True)
-	x = models.IntegerField(default=0)
-	y = models.IntegerField(default=0)
-	type = models.CharField(max_length=50)
-	def __unicode__(self):
-		return str(self.pk)
-	class Meta:
-		ordering = ['-pk']
-		get_latest_by = "pk"
+    wall = models.ForeignKey(Wall) # Probably not necessary since you have the mark/waypoint pk
+    obj_pk = models.IntegerField()
+    text = models.CharField(max_length=10000, default="", blank=True)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    type = models.CharField(max_length=50)
+    def __unicode__(self):
+        return str(self.pk)
+    class Meta:
+        ordering = ['-pk']
+        get_latest_by = "pk"
 
 class Redo(models.Model):
     '''A snapshot of a mark or waypoint containing the pk of its corresponding live object.'''
     
-	wall = models.ForeignKey(Wall)
-	obj_pk = models.IntegerField()
-	text = models.CharField(max_length=10000, default="", blank=True)
-	x = models.IntegerField(default=0)
-	y = models.IntegerField(default=0)
-	type = models.CharField(max_length=50)
-	def __unicode__(self):
-		return str(self.pk)
-	class Meta:
-		ordering = ['-pk']
-		get_latest_by = "pk"
+    wall = models.ForeignKey(Wall)
+    obj_pk = models.IntegerField()
+    text = models.CharField(max_length=10000, default="", blank=True)
+    x = models.IntegerField(default=0)
+    y = models.IntegerField(default=0)
+    type = models.CharField(max_length=50)
+    def __unicode__(self):
+        return str(self.pk)
+    class Meta:
+        ordering = ['-pk']
+        get_latest_by = "pk"
